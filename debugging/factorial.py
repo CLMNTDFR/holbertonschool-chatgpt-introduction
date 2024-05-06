075c078bac5e38bda64a7e9495f7d5d57700015c
CLMNTDFR <deferclement59@gmail.com>
2024-05-06 10:11:35 +0200

    adiing factorial file with chatgpt debogging

---
+++ debugging/factorial.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n = n - 1
+    return result
+
+
+f = factorial(int(sys.argv[1]))
+print(f)
